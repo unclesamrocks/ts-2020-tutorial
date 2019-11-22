ba9431fcf1e1efd9394465f10143bf73bc2faeb8
Nikita Moiseev <moiseev.nikita@gmail.com>
2019-11-22 10:19:00 +0300

    upd
    1. now using tsc to watch ts file (to prevent process crush on compile errors)
    2. added tsc options file
    3. continue on app

--- main/app.js
+++ main/app.js
@@ -1,8 +1,11 @@
 var root = document.getElementById('root');
-function add(n1, n2) {
+function add(n1, n2, isPrint) {
+    if (isPrint)
+        console.log(n1, n2);
     return n1 + n2;
 }
-var number1 = 6;
+var number1 = 9;
 var number2 = 2.8;
-var r = add(number1, number2);
+var isPrint = true;
+var r = add(number1, number2, isPrint);
 root.textContent = r.toString();
--- main/app.ts
+++ main/app.ts
@@ -1,12 +1,14 @@
 const root: HTMLElement = document.getElementById('root')
 
-function add(n1: number, n2: number): number {
+function add(n1: number, n2: number, isPrint: boolean): number {
+	if (isPrint) console.log(n1, n2)
 	return n1 + n2
 }
 
-const number1 = 6
+const number1 = 9
 const number2 = 2.8
+const isPrint: boolean = true
 
-const r = add(number1, number2)
+const r = add(number1, number2, isPrint)
 
 root.textContent = r.toString()
