c05b6191ade1001ae3488ceb8860a4ff156db125
Nikita Moiseev <moiseev.nikita@gmail.com>
2019-11-22 09:52:41 +0300

    ts types + styling

--- main/app.js
+++ main/app.js
@@ -1,1 +1,8 @@
 var root = document.getElementById('root');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 6;
+var number2 = 2.8;
+var r = add(number1, number2);
+root.textContent = r.toString();
--- main/app.ts
+++ main/app.ts
@@ -1,1 +1,12 @@
 const root: HTMLElement = document.getElementById('root')
+
+function add(n1: number, n2: number): number {
+	return n1 + n2
+}
+
+const number1 = 6
+const number2 = 2.8
+
+const r = add(number1, number2)
+
+root.textContent = r.toString()
