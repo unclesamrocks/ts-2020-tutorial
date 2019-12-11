3d7e0b8e451cfb7603c353b4646fbd0383cbe86b
Nikita Moiseev <moiseev.nikita@gmail.com>
2019-12-11 17:35:41 +0300

    finishing map project

--- prj-libs/src/app.ts
+++ prj-libs/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+import { searchAddressHandler } from './util/map'
+
+const form = document.querySelector('form')!
+// const addressInput = document.getElementById('address')! as HTMLInputElement
+
+form.addEventListener('submit', handleSubmitForm)
+
+function handleSubmitForm(event: Event) {
+	event.preventDefault()
+	// const enteredAddress = addressInput.value
+	searchAddressHandler(event, 'map')
+}
